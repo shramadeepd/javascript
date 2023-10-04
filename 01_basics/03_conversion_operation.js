4bbe7b736cf1caa7ca57e43d56ca7d76760fac15
shramadeep <109851887+shramadeepd@users.noreply.github.com>
2023-10-04 17:32:57 +0000

    conversion of data types

---
+++ 01_basics/03_conversion_operation.js
@@ -0,0 +1,22 @@
+let score = 33
+
+console.log(typeof score);
+
+// "33" => 33 using Number
+//"33abc"=> Nan 
+// true => 1 ; false => 0
+
+let isLoggedIn = "hello"
+
+let booleanIsLoggdIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggdIn);
+
+// 1 => true ; 0 => false 
+// " "=> false
+// " hello "=> true (non empty string)
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber);;
