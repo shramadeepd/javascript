646b5eb6eb41172f8c533405a42a5aed338e6817
shramadeep <109851887+shramadeepd@users.noreply.github.com>
2023-10-04 17:16:02 +0000

    lec2 data types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all js codes as newer version
+
+//alert('hello')  // we are using node js so it wont run and give error 
+
+//use ecma doc to read standards
+
+let name = "shramadeep"
+let age = 21
+let isLoggedIn= false
+
+// number => 2 to the power 53
+//bigint , int 
+//string => " "
+//boolean => true/false
+//null => standalone value 
+//undefined => abhi value assign nhi hui
+// symbol => used for unique
+
+
+// object
+
+console.log(typeof null)
