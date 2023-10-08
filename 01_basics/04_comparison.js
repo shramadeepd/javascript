72ebfa3ff41b3ff02666f4fc331547ec2e860b32
shramadeep <109851887+shramadeepd@users.noreply.github.com>
2023-10-08 11:42:43 +0000

    check what type of comparison to avoid

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,11 @@
+// compare only same data types 
+
+console.log(null >0);
+console.log(null == 0);
+console.log(null >= 0); // we should avoid this comparison
+
+// comparison and equality check is diff
+
+// === strict check
+console.log('2'==2);
+console.log('2'=== 2);
