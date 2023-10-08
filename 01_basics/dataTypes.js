703cdefc3d7de7688a4e677195963b4ad33a4f6b
shramadeep <109851887+shramadeepd@users.noreply.github.com>
2023-10-08 12:01:32 +0000

    learned about primitive and non primiive data type

---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,11 @@
+// watch ecma type of datatype
+// primitive- call by value
+// 7 types - string, number , boolean , null , undefined,symbol,BigInt
+
+const id = ('123')
+const anotherID = Symbol('123') // both symbols are different
+
+console.log(typeof anotherID);
+// reference type - non primitive
+// arrays , objects , functipons
+//
