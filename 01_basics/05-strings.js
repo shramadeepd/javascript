babdcd4ea40a1cdc10b9117e0bd9bcee36419ae5
shramadeep <109851887+shramadeepd@users.noreply.github.com>
2023-10-09 10:58:44 +0000

    strings methods

---
+++ 01_basics/05-strings.js
@@ -0,0 +1,15 @@
+const name = 'shramadeep'
+const repocount = 50
+
+//console.log(name + repocount + " value"); // not recommended
+console.log(`my name is ${name} and my repo count is ${repocount}`);
+
+
+const gameName = new String("shramadeep")
+
+console.log(gameName[2]);
+console.log(gameName.length);
+
+//substring , slice ( can take negative indexing)
+// trim (removes blank spaces from first and last )
+// replace 
